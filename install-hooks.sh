c91e5daad126467b391fda57fb9de774ed518251
Alvaro Mateo <alvaromateo9@gmail.com>
2024-04-14 16:41:43 +0200

    [VLMD-18] Add prepare-commit-msg and commit-msg hooks

---
+++ install-hooks.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# To install the git hooks in your current project execute the following script from the project's top directory.
+# With the following command you will avoid having to clone the project by yourself/
+# curl -s URL | bash -s
+
