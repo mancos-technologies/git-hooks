8fa2477ef98f7b6a04964e8c9603df6c89a00d7f
Alvaro Mateo <alvaromateo9@gmail.com>
2024-04-15 17:21:20 +0200

    [VLMD-18] Fix installation script

--- install-hooks.sh
+++ install-hooks.sh
@@ -4,7 +4,7 @@
 # With the following command you will avoid having to clone the project by yourself/
 # curl -s https://raw.githubusercontent.com/mancos-technologies/git-hooks/main/install-hooks.sh | bash -s
 
-git clone git@github.com:mancos-technologies/git-hooks.git /tmp
+git clone git@github.com:mancos-technologies/git-hooks.git /tmp/git-hooks
 chmod +x /tmp/git-hooks/hooks/*
 cp /tmp/git-hooks/hooks/* .git/hooks
 rm -rf /tmp/git-hooks
